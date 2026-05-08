5fe1540fda870b05d1f67b2cd322ef2d00d3e8d5
Nitish Sharma <0007nitishsharma@gmail.com>
2026-05-08 16:25:33 +0530

    Code: added code for model validations

---
+++ app/models/employee.rb
@@ -0,0 +1,9 @@
+class Employee < ApplicationRecord
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :first_name, :job_title, :country, :department, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  validates :employee_code, presence: true, uniqueness: true
+  validates :salary, presence: true, numericality: { greater_than: 0 }
+  validates :hire_date, presence: true 
+end
